5e1298b4e028d763324a09e70106f4efa35f5e37
Navghare_Vaibhav <navgharevaibhav5@gmail.com>
2025-04-06 17:59:46 +0530

    project version 1.1 deply

--- lib/Admin/Admin_home.dart
+++ lib/Admin/Admin_home.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import '../screens/home_screen/drawer_side.dart';
- // Import the drawer widget
+import '../screens/home_screen/drawer_side.dart'; // Import the drawer widget
 
 class AdminPanel extends StatefulWidget {
   @override
@@ -12,6 +11,7 @@ class _AdminPanelState extends State<AdminPanel> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _priceController = TextEditingController();
   final TextEditingController _imageUrlController = TextEditingController();
+  final TextEditingController _stockController = TextEditingController(); // New
 
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
@@ -27,29 +27,38 @@ class _AdminPanelState extends State<AdminPanel> {
     String name = _nameController.text.trim();
     double? price = double.tryParse(_priceController.text.trim());
     String imageUrl = _imageUrlController.text.trim();
+    int? stock = int.tryParse(_stockController.text.trim()); // New
 
-    if (name.isNotEmpty && price != null && imageUrl.isNotEmpty) {
+    if (name.isNotEmpty && price != null && imageUrl.isNotEmpty && stock != null) {
       _firestore.collection(_selectedCollection).add({
         'productName': name,
         'productPrice': price,
         'productImage': imageUrl,
+        'stock': stock, // New field
       }).then((documentReference) {
-        _firestore.collection(_selectedCollection).doc(documentReference.id).update({
+        _firestore
+            .collection(_selectedCollection)
+            .doc(documentReference.id)
+            .update({
           'productId': documentReference.id,
         });
 
         _nameController.clear();
         _priceController.clear();
         _imageUrlController.clear();
+        _stockController.clear(); // Clear new field
         ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Product added successfully!')));
+          SnackBar(content: Text('Product added successfully!')),
+        );
       }).catchError((error) {
         ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text('Error adding product: $error')));
+          SnackBar(content: Text('Error adding product: $error')),
+        );
       });
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Please fill in all fields correctly.')));
+        SnackBar(content: Text('Please fill in all fields correctly.')),
+      );
     }
   }
 
@@ -59,25 +68,23 @@ class _AdminPanelState extends State<AdminPanel> {
       appBar: AppBar(
         title: Text('Admin Panel'),
       ),
-      // drawer: DrawerSide(), // Add the drawer here
+      // drawer: DrawerSide(), // Add the drawer here if needed
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             DropdownButton<String>(
               value: _selectedCollection,
-              items: _collections.map((String collection) {
+              items: _collections.map((String value) {
                 return DropdownMenuItem<String>(
-                  value: collection,
-                  child: Text(collection),
+                  value: value,
+                  child: Text(value),
                 );
               }).toList(),
               onChanged: (String? newValue) {
-                if (newValue != null) {
-                  setState(() {
-                    _selectedCollection = newValue;
-                  });
-                }
+                setState(() {
+                  _selectedCollection = newValue!;
+                });
               },
             ),
             TextField(
@@ -86,14 +93,19 @@ class _AdminPanelState extends State<AdminPanel> {
             ),
             TextField(
               controller: _priceController,
-              keyboardType: TextInputType.number,
               decoration: InputDecoration(labelText: 'Product Price'),
+              keyboardType: TextInputType.number,
             ),
             TextField(
               controller: _imageUrlController,
-              decoration: InputDecoration(labelText: 'Product Image URL'),
+              decoration: InputDecoration(labelText: 'Image URL'),
             ),
-            SizedBox(height: 20),
+            TextField(
+              controller: _stockController,
+              decoration: InputDecoration(labelText: 'Stock'), // New field
+              keyboardType: TextInputType.number,
+            ),
+            SizedBox(height: 16),
             ElevatedButton(
               onPressed: _addProduct,
               child: Text('Add Product'),
@@ -104,3 +116,113 @@ class _AdminPanelState extends State<AdminPanel> {
     );
   }
 }
+
+
+
+
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:flutter/material.dart';
+// import '../screens/home_screen/drawer_side.dart';
+//  // Import the drawer widget
+//
+// class AdminPanel extends StatefulWidget {
+//   @override
+//   _AdminPanelState createState() => _AdminPanelState();
+// }
+//
+// class _AdminPanelState extends State<AdminPanel> {
+//   final TextEditingController _nameController = TextEditingController();
+//   final TextEditingController _priceController = TextEditingController();
+//   final TextEditingController _imageUrlController = TextEditingController();
+//
+//   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+//
+//   final List<String> _collections = [
+//     'FruitsProduct',
+//     'GrainsProduct',
+//     'VegetablesProduct'
+//   ];
+//
+//   String _selectedCollection = 'FruitsProduct';
+//
+//   void _addProduct() {
+//     String name = _nameController.text.trim();
+//     double? price = double.tryParse(_priceController.text.trim());
+//     String imageUrl = _imageUrlController.text.trim();
+//
+//     if (name.isNotEmpty && price != null && imageUrl.isNotEmpty) {
+//       _firestore.collection(_selectedCollection).add({
+//         'productName': name,
+//         'productPrice': price,
+//         'productImage': imageUrl,
+//       }).then((documentReference) {
+//         _firestore.collection(_selectedCollection).doc(documentReference.id).update({
+//           'productId': documentReference.id,
+//         });
+//
+//         _nameController.clear();
+//         _priceController.clear();
+//         _imageUrlController.clear();
+//         ScaffoldMessenger.of(context).showSnackBar(
+//             SnackBar(content: Text('Product added successfully!')));
+//       }).catchError((error) {
+//         ScaffoldMessenger.of(context).showSnackBar(
+//             SnackBar(content: Text('Error adding product: $error')));
+//       });
+//     } else {
+//       ScaffoldMessenger.of(context).showSnackBar(
+//           SnackBar(content: Text('Please fill in all fields correctly.')));
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Admin Panel'),
+//       ),
+//       // drawer: DrawerSide(), // Add the drawer here
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
+//         child: Column(
+//           children: [
+//             DropdownButton<String>(
+//               value: _selectedCollection,
+//               items: _collections.map((String collection) {
+//                 return DropdownMenuItem<String>(
+//                   value: collection,
+//                   child: Text(collection),
+//                 );
+//               }).toList(),
+//               onChanged: (String? newValue) {
+//                 if (newValue != null) {
+//                   setState(() {
+//                     _selectedCollection = newValue;
+//                   });
+//                 }
+//               },
+//             ),
+//             TextField(
+//               controller: _nameController,
+//               decoration: InputDecoration(labelText: 'Product Name'),
+//             ),
+//             TextField(
+//               controller: _priceController,
+//               keyboardType: TextInputType.number,
+//               decoration: InputDecoration(labelText: 'Product Price'),
+//             ),
+//             TextField(
+//               controller: _imageUrlController,
+//               decoration: InputDecoration(labelText: 'Product Image URL'),
+//             ),
+//             SizedBox(height: 20),
+//             ElevatedButton(
+//               onPressed: _addProduct,
+//               child: Text('Add Product'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
